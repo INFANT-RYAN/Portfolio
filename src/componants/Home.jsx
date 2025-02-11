d60d150abdbf0ef3ede1991b0edf0c77460caea2
Infant-Ryan <ini0k17@gmail.com>
2025-02-11 20:48:29 +0530

    fix: home page typogtraphy

--- src/componants/Home.jsx
+++ src/componants/Home.jsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <div className="bg-black container  p-7 md:py-44 text-white flex justify-center">
       <div className="w-[70%]  leading-loose">
-        <h2 className="text-4xl font-bold pb-2.5">
+        <h2 className="md:text-4xl text-2xl font-bold pb-2.5">
           <span className="text-[#8137db]">Welcome</span> to My Portfolio!
         </h2>
 
-        <h3 className="font-bold text-lg pb-4">
+        <h3 className="font-bold md:text-lg text-sm pb-4">
           &#128075; Hello!{" "}
           <span className="text-[#8137db]">
             {" "}
@@ -18,7 +18,7 @@ export default function Home() {
           </span>
           ,
         </h3>
-        <p className="text-xl ">
+        <p className="md:text-xl ">
           <span className="md:pl-16  font-semibold text-gray-500">
           Passionate computer science student with skills in programming and hardware interfacing, driven to create innovative solutions.
           </span>
