d217924717c6a023501356a5127600479b58699b
Infant-Ryan <ini0k17@gmail.com>
2025-02-11 20:38:00 +0530

    chore: resp nav bar

--- src/componants/Home.jsx
+++ src/componants/Home.jsx
@@ -2,13 +2,13 @@ import { ReactTyped } from "react-typed";
 
 export default function Home() {
   return (
-    <div className="bg-black container mx-[100] p-7 text-white flex justify-center">
-      <div className="w-[70%] leading-loose">
+    <div className="bg-black container mx-[100] p-7 md:py-44 text-white flex justify-center">
+      <div className="w-[70%]  leading-loose">
         <h2 className="text-4xl font-bold pb-2.5">
           <span className="text-[#8137db]">Welcome</span> to My Portfolio!
         </h2>
 
-        <h3 className="font-semibold text-lg pb-4">
+        <h3 className="font-bold text-lg pb-4">
           &#128075; Hello!{" "}
           <span className="text-[#8137db]">
             {" "}
@@ -18,13 +18,9 @@ export default function Home() {
           </span>
           ,
         </h3>
-        <p className="text-xl">
-          <span className="pl-16">
-            A passionate and dedicated computer science student at St Joseph's
-            College (Autonomous), Tiruchirapalli, expected to graduate in May
-            2025. With a solid foundation in programming and hardware
-            interfacing, I am driven by the desire to build innovative solutions
-            and make a positive impact.
+        <p className="text-xl ">
+          <span className="md:pl-16  font-semibold text-gray-500">
+          Passionate computer science student with skills in programming and hardware interfacing, driven to create innovative solutions.
           </span>
         </p>
       </div>
--- src/componants/navbar.jsx
+++ src/componants/navbar.jsx
@@ -1,9 +1,37 @@
+import { MdMenu } from "react-icons/md";
+
 export default function Nav() {
   return (
-    <div className="bg-black p-6 flex justify-between px-8 container">
-      <div></div>
-      <nav>
-        <ul className="flex space-x-4 text-white ">
+    <div className="bg-black p-6 container border-b border-gray-600">
+      <div className=" flex justify-between px-8 ">
+        <div></div>
+        <nav>
+          <div className="hidden md:block">
+            <ul className="flex space-x-4 text-white  ">
+              <li className="hover:text-[#09ebdf] text-gray-300 font-semibold">
+                <a href="#">Home</a>
+              </li>
+              <li className="hover:text-[#09ebdf] text-gray-300 font-semibold">
+                <a href="#">About</a>
+              </li>
+              <li className="hover:text-[#09ebdf] text-gray-300 font-semibold">
+                <a href="#">Education</a>
+              </li>
+              <li className="hover:text-[#09ebdf] text-gray-300 font-semibold">
+                <a href="#">Project</a>
+              </li>
+              <li className="hover:text-[#09ebdf] text-gray-300 font-semibold">
+                <a href="#">Experience</a>
+              </li>
+            </ul>
+          </div>
+          <div className="text-white md:hidden text-2xl menu-ico">
+            <MdMenu />
+          </div>
+        </nav>
+      </div>
+      <div className="md:hidden mob-menu">
+        <ul className="flex flex-col items-center space-x-4 text-white  ">
           <li className="hover:text-[#09ebdf] text-gray-300 font-semibold">
             <a href="#">Home</a>
           </li>
@@ -20,7 +48,7 @@ export default function Nav() {
             <a href="#">Experience</a>
           </li>
         </ul>
-      </nav>
+      </div>
     </div>
   );
 }
